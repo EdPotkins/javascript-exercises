c74199aeb63b09855d0e14a20a01068745e5e337
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-07 14:39:37 +0300

    First 2 tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, valueToRemove, ...otherValuesToRemove) {
+    
+    let indexArray = [];
+    for (let i = 1; i < arguments.length; i++){
+        indexArray.push(givenArray.indexOf(arguments[i]));
+    }
+    for (let i = 0; i < arguments.length - 1; i++){
+        givenArray.splice(indexArray[i], 1)
+    }
 
+    return givenArray;
 };
 
 // Do not edit below this line
