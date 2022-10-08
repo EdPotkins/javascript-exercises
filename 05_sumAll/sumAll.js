26495df37e85f0a88bb60733b899d20d1a489d29
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-08 09:39:34 +0300

    Passes first two tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, endInt) {
+    let finalSum = 0;
+    for (startInt; startInt <= endInt; startInt++){
+        finalSum += startInt;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
