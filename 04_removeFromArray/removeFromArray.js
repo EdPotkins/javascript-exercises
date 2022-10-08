40d4348683fed420ff44148c11f7d1dfb6c71cd9
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-08 09:22:13 +0300

    Implement other solution, all tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,15 @@
-const removeFromArray = function(givenArray, valueToRemove, ...otherValuesToRemove) {
+function removeFromArray(...args){
 
- //check if valueToRemove is present in givenArray with method .some()
- //put the result of this check (true/false) into a varible isPresent
- //if isPresent true then run code, else dont compare
-    let isPresent = givenArray.some((element) => element === valueToRemove);
-    if(isPresent){
-        let indexArray = [];
-        for (let i = 1; i < arguments.length; i++){
-            indexArray.push(givenArray.indexOf(arguments[i]));
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
         }
-        for (let i = 0; i < arguments.length - 1; i++){
-            givenArray.splice(indexArray[i], 1)
-        }
+    });
+    return newArray;
 
-        return givenArray;
-    } else return givenArray;
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
