a52ef699327dd5827257d87839544a421faec6ab
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-08 08:41:30 +0300

    Ignore non present values test pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,20 @@
 const removeFromArray = function(givenArray, valueToRemove, ...otherValuesToRemove) {
-    
-    let indexArray = [];
-    for (let i = 1; i < arguments.length; i++){
-        indexArray.push(givenArray.indexOf(arguments[i]));
-    }
-    for (let i = 0; i < arguments.length - 1; i++){
-        givenArray.splice(indexArray[i], 1)
-    }
 
-    return givenArray;
+ //check if valueToRemove is present in givenArray with method .some()
+ //put the result of this check (true/false) into a varible isPresent
+ //if isPresent true then run code, else dont compare
+    let isPresent = givenArray.some((element) => element === valueToRemove);
+    if(isPresent){
+        let indexArray = [];
+        for (let i = 1; i < arguments.length; i++){
+            indexArray.push(givenArray.indexOf(arguments[i]));
+        }
+        for (let i = 0; i < arguments.length - 1; i++){
+            givenArray.splice(indexArray[i], 1)
+        }
+
+        return givenArray;
+    } else return givenArray;
 };
 
 // Do not edit below this line
