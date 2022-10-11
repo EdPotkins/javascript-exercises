0d7c48fc6b9661130ea125ff8dff0a7dc755e0fa
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-11 10:27:56 +0300

    Pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const initialValue = 0;
+  const sumWithInitial = array.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+  return sumWithInitial;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const initialValue = 1;
+  const productWithInitial = array.reduce(
+    (previousValue, currentValue) => previousValue * currentValue,
+    initialValue
+  );
+  return productWithInitial;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (let i = num - 1; i >= 1; i--){
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
