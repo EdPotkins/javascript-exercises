2a67a6ba79f3149b31264348b4291978c40902f9
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-11 16:34:44 +0300

    Pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(term) {
+    let n1 = 1, n2 = 1, nextTerm;
+    let sequence = [];
+    let parsedTerm = parseInt(term);
+    if (term > 0){
+        for (let i = 1; i <= parsedTerm; i++){
+            sequence.push(n1);
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+        }
+        return sequence[parsedTerm-1];
+    } else return "OOPS";  
+}    
 
 // Do not edit below this line
 module.exports = fibonacci;
