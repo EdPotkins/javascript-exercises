ca3aa442f9715d511bb02592e80a6347c8eb4ce1
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-13 20:10:59 +0300

    Pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfObjects) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
 
+    const oldest = arrayOfObjects.reduce((previousValue, currentValue) => {
+        if(typeof previousValue.yearOfDeath !== 'undefined'){
+            const previousAge = previousValue.yearOfDeath - previousValue.yearOfBirth;
+            const currentAge = currentValue.yearOfDeath - currentValue.yearOfBirth;
+            return previousAge < currentAge ? currentValue : previousValue;
+        } else {
+            const previousAge = currentYear - previousValue.yearOfBirth;
+            const currentAge = currentValue.yearOfDeath - currentValue.yearOfBirth;
+            return previousAge < currentAge ? currentValue : previousValue;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
